7b5ec0ce2356438b9e0bff06a772df82991f5142
Alexandra <alexh88@ucla.edu>
2019-10-15 22:02:17 -0700

    FINALLY got all the logos to work

--- js/sponsors.js
+++ js/sponsors.js
@@ -2,7 +2,7 @@
     {
         src: "mediacom.png",
         alt: "MediaCom",
-        url: "mediacom.com",
+        url: "www.mediacom.com",
         body: "MediaCom helps brands unlock growth through media by applying our Systems Thinking approach. As part of WPP and GroupM, we have access to the richest data and most robust benchmarks in the business. Our success is underpinned by our belief “People First, Better Results” that if we invest in our people, we’ll deliver better results."
     }
 ];
@@ -11,174 +11,145 @@ var goldSponsors = [
   {
       src: "FarApp.png",
       alt: "FarApp",
-      url: "farapp.com",
+      url: "www.farapp.com",
       body: "FarApp is SAAS e-commerce automation company. We began in 2004 as a one man operation pulling orders from Amazon and pushing them into NetSuite for a nominal subscription. Since that time as e-commerce and NetSuite have grown, so have we. We now connect to over 60 different channels and service hundreds of clients, automating every part of their e-commerce experience. We are trying to aggressively grow our team to meet our ongoing customer demands. We hope that you will be a great fit for our organization!",
   },
   {
       src: "nimble.png",
       alt: "Nimble",
-      url: "nimble.com",
+      url: "www.nimble.com",
       body: "Nimble is the simple, smart CRM for Office 365 and G Suite teams."
   },
   {
       src: "usaepay.png",
       alt: "USAePay",
-      url: "usaepay.info",
+      url: "www.usaepay.info",
       body: "Payment solution for business owners to accept payment from clients"
   },
   {
       src: "gumgum.png",
       alt: "GumGum",
-      url: "gumgum.com",
+      url: "www.gumgum.com",
       body: "GumGum is an artificial intelligence company with a focus on computer vision. Our mission is to solve hard problems across a variety of industries by teaching machines to see and understand the world. Since 2008, the company has applied its patented capabilities to serve a variety of industries from advertising to professional sports, with more to come."
   },
   {
       src: "NuORDER.png",
       alt: "NuORDER",
-      url: "nuorder.com",
+      url: "www.nuorder.com",
       body: "NuORDER is a venture-backed, B2B eCommerce technology company revolutionizing the way the $130 billion wholesale business is done. Since 2011, we’ve offered a platform to empower brands and retailers to conduct their business in a smarter, modern, more efficient way and 100% online."
   },
  {
       src: "disqo.png",
       alt: "DISQO",
-      url: "disqo.com",
+      url: "www.disqo.com",
       body: "DISQO is a consumer-first insights platform that connects what people say and do to provide unprecedented insights for the market research industry.  By engaging our first-party research audience to share their attitudes and behaviors, DISQO captures the highest quality data, empowering its clients to make confident decisions."
   },
   {
       src: "ephesoft.png",
       alt: "Ephesoft",
-      url: "esphesoft.com",
+      url: "www.esphesoft.com",
       body: "Ephesoft makes meaning out of unstructured data through document capture and extraction backed up by practical experience and solid technology. Ephesoft drives automation through the Cloud, and Ephesoft is committed to leading the market with revolutionary technology that combines content automation, AI and machine learning."
   }
 ];
 
 var bronzeSponsors = [
   {
-      src: "shoobilee.png",
-      alt: "Shoobilee Inc.",
-      url: "www.shoobilee.com",
-      body: "We at Shoobilee believe in changing the world through fashion, one sole at a time. With our unique unlimited footwear subscription service, Shoobilee is bridging the constant desire for new shoes to the endless demand from charity.  Imagine having access to a dream shoe closet at an affordable monthly price, simply select, wear, & return the pair to us when you are done. We will donate that pair to our charity partners and send you your next brand new pair, and there is no limit on how many cycles per month! Giving back has never looked so good!",
-      positions: "Marketing intern, backend software engineer full-time/part-time/intern"
+      src: "getaround.png",
+      alt: "Getaround",
+      url: "www.getaround.com",
+      body: "Getaround is a carsharing platform powered by the Getaround Connect® technology which provides drivers access to rent and unlock cars shared by people nearby."
   },
   {
-      src: "discotech.jpg",
-      alt: "Discotech",
-      url: "app.discotech.me",
-      body: "OpenTable for Nightlife. We make it easier for customers to discover events, buy tickets, reserve tables, and sign up for free guest lists at nightlife venues in different cities.",
-      positions: "Marketing intern, social media / content intern"
+      src: "teamplayer.jpg",
+      alt: "TeamPlayerHR",
+      url: "www.teamplayerhr.com",
+      body: "TeamPlayerHR is a revolutionary website tool that dramatically reduces the problem of bad hires, and poor team member compatibility. TeamPlayerHR has its clear focus on helping employers drive high performance team building and retention. It has been estimated that bad hires and poor team member compatibility can cost £32,000 per employee in recruitment fees, wasted wages, loss of productivity and time. It is further estimated that management and supervisors can waste up to 46% of their time on HR issues and that bad compatibility and fit mean that a high percentage of employees may be seeking new jobs at any one time. "
   },
   {
-      src: "stratalux.png",
-      alt: "Stratalux",
-      url: "www.stratalux.com",
-      body: "Stratalux was founded in 2010 as a born-in-the-cloud Managed Services Provider that focuses exclusively on AWS and DevOps. As an audited AWS Next-Gen Managed Services Provider and AWS Advanced Consulting Partner, Stratalux helps clients build and manage solutions built on Amazon Web Services. ",
-      positions: "Junior Cloud DevOps Engineer FT, Junior Cloud Operations Engineer FT, Senior Product Owner FT, Digital Marketing Manager FT"
+      src: "springlabs.jpg",
+      alt: "Spring Labs",
+      url: "www.springlabs.com",
+      body: "Spring Labs is building a protocol focused on protecting consumer data for the new age of information exchange and privacy."
   },
   {
-      src: "pwrdby.png",
-      alt: "PwrdBy",
-      url: "pwrdby.com",
-      body: "PwrdBy develops web and mobile software for non profits and hospitals.",
-      positions: "Sales, Marketing, Engineering (full time and interns)"
+      src: "sabio.png",
+      alt: "Sabio Coding Bootcamp",
+      url: "www.sabio.la",
+      body: "Sabio Coding Bootcamp"
   },
   {
-      src: "tigertext.png",
-      alt: "TigerText",
-      url: "www.tigertext.com/",
-      body: "As healthcare’s largest provider of clinical communication solutions, TigerText helps physicians, nurses, and other staff communicate and collaborate more effectively, accelerating productivity, reducing costs, and improving patient outcomes.",
-      positions: "iOS Developer Intern, Web Developer Intern, Data Intern"
+      src: "freezecrowd.jpg",
+      alt: "FreezeCrowd, Inc.",
+      url: "www.freezecrowd.com",
+      body: "FreezeCrowd is a unique social media platform and network for college students and alumni that connects you in photos with friends! It's cool or unique because everyone has an ice cube over their profile picture, and you 'break the ice' when visiting a friends profile.  By breaking the ice, you can ask a question or play an educational game, among other interesting applications that can be built over the break the ice feature.  FreezeCrowd is similar to a school yearbook in some ways, yet private to outside searches.  Every photo is unique like a snowflake and categorized!"
   },
   {
-      src: "localroots.png",
-      alt: "Local Roots Farms",
-      url: "www.localrootsfarms.com",
-      body: "At Local Roots, we design, build and operate the world’s most productive indoor farming solutions. We believe the key to a more sustainable future requires eliminating supply-chain risks and undoing the commoditization of the food industry. That’s why our Local Roots Family is building a distributed network of cutting-edge farms throughout the world to grow the freshest, healthiest produce possible.",
-      positions: "Farm Operations, Mechanical Engineer, Software Engineer"
+      src: "clutter.png",
+      alt: "Clutter",
+      url: "www.clutter.com",
+      body: "Clutter is an on-demand, technology company based in Los Angeles that is disrupting the $50B/year self-storage and moving industries. We’ve built an end-to-end logistics and supply chain platform that enables us to offer consumers a much more convenient solution at price parity with the incumbents. We’ve raised $300M from a number of VCs, including SoftBank, Sequoia, Atomico and GV (formerly Google Ventures). We have 800+ team members and tens of thousands of customers in 7 major markets across the US with plans to be in 50+ markets, domestically and internationally, within the next 5 years! At Clutter, we're fortunate to be providing a consumer value proposition that people love and one that makes economic sense - a true product/market fit that few startups ever find. To deliver on our promise to consumers, team members and investors, we're focused on hiring, training and retaining exceptional individuals. This means that we have a very thorough interview process and maintain high performance expectations, but we'll always be transparent with you and respectful of your time."
   },
   {
-      src: "expertdojo.jpg",
-      alt: "Expert DOJO",
-      url: "expertdojo.com",
-      body: "We are an early stage accelerator for startups to grow.",
-      positions: "Sales manager, admin, digital marketer"
-  },
-  {
-      src: "aerserv.png",
-      alt: "AerServ",
-      url: "www.aerserv.com",
-      body: "AerServ is the leading inventory and audience management technology for mobile publishers and advertisers, specializing in mobile video technology, with proprietary yield optimization tools that increase revenue across all formats and ad units.",
-      positions: "Business Development Associates, Account Manager, Account Executive, SDK Developer, Java Engineer"
-  },
-  {
-      src: "trip.png",
-      alt: "Trip.com",
-      url: "trip.com",
-      body: "Trip.com is an award winning travel app and website that helps people discover great places to stay, eat and play that are perfect for them. Our unique combination of predictive AI technology and personalization make finding and having great experiences, whether local or global, easier than ever.",
-      positions: "LA Ambassadors, LA City Editor"
-  },
-  {
-      src: "playfull.png",
-      alt: "Playfull",
-      url: "beplayfull.com",
-      body: "Play games and win food at local restaurants!",
-      positions: "Marketing Intern, Software Engineer Intern - anytime"
+      src: "blisspoint.png",
+      alt: "Bliss Point Media",
+      url: "www.blisspointmedia.com",
+      body: "Bliss Point Media is a rapidly growing performance media advertising agency, based in Santa Monica that combines world-class media capabilities with cutting-edge statistics and computer science. Using technology as a differentiator, Bliss Point Media brings modern statistical and computational techniques to bear on our clients’ biggest marketing opportunities across linear TV and streaming/OTT video platforms. Our approach provides our clients with unique and powerful tools to maximize the effectiveness of their advertising campaigns while providing an experience of strategic depth and flawless execution."
   },
   {
       src: "ariento.png",
-      alt: "Ariento",
+      alt: "Ariento Inc.",
       url: "www.ariento.com",
-      body: "Ariento provides secure technology solutions to small businesses and individuals. Specifically, our core services of Cyber Lookout, Secure Workstation, and Secure Office.",
-      positions: "Cybersecurity Intern, Security Operations Intern, Penetration Test Intern, Business Development Intern, Marketing Intern, Finance Intern"
+      body: "Ariento provides secure technology solutions to small businesses and individuals. Specifically, our core services of Cyber Lookout, Secure Workstation, and Secure Office."
   },
   {
-      src: "collegevisitco.png",
-      alt: "College Visit Co.",
-      url: "www.collegevisit.co",
-      body: "College Visit Co. produces student-written e-book guides for parents of prospective students on what to see, eat, and do when making college visits.",
-      positions: "Marketing Intern, Social Media Intern, Editorial/Blog Intern, Growth Hacking Intern, Wordpress Intern, Outreach/Partnership Intern"
+      src: "tigerconnect.png",
+      alt: "TigerConnect",
+      url: "www.tigerconnect.com",
+      body: "As healthcare’s largest provider of clinical communication solutions, TigerConnect helps physicians, nurses, and other staff communicate and collaborate more effectively, accelerating productivity, reducing costs, and improving patient outcomes. With over 6,000 facilities, delivering hundreds of millions of messages monthly, TigerConnect continually delivers advanced product innovations and integrates with critical hospital systems such as the EHR, nurse call, and scheduling solutions."
   },
   {
-      src: "puffpack.png",
-      alt: "Puff Pack",
-      url: "thepuffpack.com",
-      body: "Started from my dorm at UCLA, Puff Pack is a monthly smoking supply delivery service that sends out thousands of packages of smoking supplies to smokers all around the world. Customer can build custom smoking packs or choose from our curated packs.",
-      positions: "CFO, Marketing Manager, Content Creator"
+      src: "onebox.png",
+      alt: "OneBox LTD",
+      url: "www.onebox.com/signup/",
+      body: "OneBox uses AI SaaS to increase container shipping efficiency by 20%, increasing logistics company profitability and reducing carbon emissions on a global scale."
   },
   {
-      src: "p&p.png",
-      alt: "Phoenix&Powell",
-      url: "phoenixpowell.org",
-      body: "We are a consulting company aimed at High Growth LA Startups. Since launching earlier this year, we have worked with companies such as Skurt, Unmute, Gem, and DApper Network, which have raised funding from top investors such as Greylock, Upfront, and First Round Capital.",
-      positions: "Associate consultants, project leads"
+      src: "deep6AI.png",
+      alt: "Deep 6 AI",
+      url: "www.deep6.ai",
+      body: "We are an AI company that has created a new industry called Clinical Trial Acceleration Software.  We help bring life-saving drugs to market faster and lower the costs of these medications.  We just made Forbes 2019 Top 50 AI Companies list, we have a great development culture, a team of top talent, we just raised our Series A, and we are about to open our new office in Pasadena."
   },
   {
-      src: "textpert.png",
-      alt: "Textpert",
-      url: "www.textpert.ai",
-      body: "Textpert is AI for subjective problems. Our vision: use AI to enhance the way the world solves interpersonal problems and mental health issues. Textpert will allow humans to ask private and nuanced questions to an AI entity for the first time.",
-      positions: "Part-time machine learning engineering intern (Winter/Spring), Full-time machine learning engineering intern (Summer), Part-time iOS front-end engineering intern (Winter/Spring), Full-time iOS front-end engineering intern (Summer)"
+      src: "discotech.png",
+      alt: "Discotech",
+      url: "www.discotech.me",
+      body: "Discotech is the OpenTable for Nightlife. Our free mobile app and website make it easier for customers to experience 1000+ nightlife venues across 45+ cities."
   },
   {
-      src: "tutorfly.png",
-      alt: "Tutorfly Inc.",
-      url: "tutorfly.org",
-      body: "Tutorfly is a peer to peer tutoring platform that is more effective and affordable than existing options.  We are rapidly growing and have a presence in the Bay Area, Los Angeles, and San Diego!",
-      positions: "Software engineering full-time/part-time/intern, Marketing Intern, Business Development Intern"
+      src: "need.png",
+      alt: "Need Westwood",
+      url: "www.needwestwood.com",
+      body: "We deliver your favorite snacks in under 10 minutes using electric vehicles! Customers order on our website needwestwood.com while we are building the app."
   },
   {
-      src: "rollingrobots.jpg",
-      alt: "Rolling Robots Inc.",
-      url: "RollingRobots.com",
-      body: "Rolling Robots provides the robotic entertainment and learning experience for kids age 4 to 16. We provide the most unique robot building, battling and coding activities - birthday party, play-date, workshop, field trip, competition team, and robotics and STEM camp.",
-      positions: "Robotics Instructor - full-time/part-time, Winter/Summer; Party Host - part-time/weekend, year round; Marketing Intern - part-time"
+      src: "sike.png",
+      alt: "Sike AI",
+      url: "www.sikeinsights.com",
+      body: "Sike is an enterprise software company that's combining psychology and machine learning to help teams work better together."
   },
   {
-      src: "pluto.png",
-      alt: "Pluto Money",
-      url: "www.gopluto.io",
-      body: "The best money management app for college students, supported by Queen City FinTech Incubator and Startup UCLA Accelerator! Achieve your saving goals by doing simple challenges tailored to your finances. Compare your finances & spending habits to similar peers, even on the same campus. Everything is automatically tracked for you with bank-level security.",
-      positions: "Marketing intern (ASAP, Winter Q), Campus ambassadors (ASAP, Winter Q), Part-time front-end development intern (ASAP, Winter Q)"
+      src: "PEARL.png",
+      alt: "Pearl",
+      url: "www.trypearl.io",
+      body: "Pearl is a job platform for young professionals to discover, follow, & apply to amazing new companies; and for those companies to represent themselves with more transparent information about their values, culture, and mission statement."
+  },
+ {
+      src: "textbookdibs.png",
+      alt: "TextbookDibs",
+      url: "www.textbook-dibs.herokuapp.com",
+      body: "Textbook Dibs is the most convenient way to resell your textbooks."
   }
+
 ];
 
 function eachFun(company, title) {
@@ -191,7 +162,7 @@ function eachFun(company, title) {
             "<p>` + company.body + ` Learn more at
             <a style='word-wrap: break-word;' target='_blank' href='//` + company.url +`'> ` + company.url +`</a>.</p>
             <p>Positions: ` + company.positions + `</p>" data-original-title="` + company.alt + `">
-          <img src="../assets/logos/` + company.src +  `" alt="` + company.alt + `"/></a>
+          <img src="assets/logos/` + company.src +  `" alt="` + company.alt + `"/></a>
       </div>`;
   }
   else {
@@ -202,7 +173,7 @@ function eachFun(company, title) {
           "<p>` + company.body + ` Learn more at
           <a style='word-wrap: break-word;' target='_blank' href='//` + company.url +`'> ` + company.url +`</a>.</p>"
           data-original-title="` + company.alt + `">
-        <img src="../assets/logos/` + company.src +  `" alt="` + company.alt + `"/></a>
+        <img src="assets/logos/` + company.src +  `" alt="` + company.alt + `"/></a>
     </div>`;
   }
 }
