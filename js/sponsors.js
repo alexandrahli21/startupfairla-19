27b05d96ba4fbe8f65845611b275c49838a95000
Alexandra <alexh88@ucla.edu>
2019-10-17 10:28:58 -0700

    fixed company links

--- js/sponsors.js
+++ js/sponsors.js
@@ -2,7 +2,7 @@
     {
         src: "mediacom.png",
         alt: "MediaCom",
-        url: "https://www.mediacom.com",
+        url: "www.mediacom.com",
         body: "MediaCom helps brands unlock growth through media by applying our Systems Thinking approach. As part of WPP and GroupM, we have access to the richest data and most robust benchmarks in the business. Our success is underpinned by our belief “People First, Better Results” that if we invest in our people, we’ll deliver better results."
     }
 ];
@@ -11,43 +11,43 @@ var goldSponsors = [
   {
       src: "FarApp.png",
       alt: "FarApp",
-      url: "https://www.farapp.com",
+      url: "www.farapp.com",
       body: "FarApp is SAAS e-commerce automation company. We began in 2004 as a one man operation pulling orders from Amazon and pushing them into NetSuite for a nominal subscription. Since that time as e-commerce and NetSuite have grown, so have we. We now connect to over 60 different channels and service hundreds of clients, automating every part of their e-commerce experience. We are trying to aggressively grow our team to meet our ongoing customer demands. We hope that you will be a great fit for our organization!",
   },
   {
       src: "nimble.png",
       alt: "Nimble",
-      url: "https://www.nimble.com",
+      url: "www.nimble.com",
       body: "Nimble is the simple, smart CRM for Office 365 and G Suite teams."
   },
   {
       src: "usaepay.png",
       alt: "USAePay",
-      url: "https://www.usaepay.info",
+      url: "www.usaepay.info",
       body: "Payment solution for business owners to accept payment from clients"
   },
   {
       src: "gumgum.png",
       alt: "GumGum",
-      url: "https://www.gumgum.com",
+      url: "www.gumgum.com",
       body: "GumGum is an artificial intelligence company with a focus on computer vision. Our mission is to solve hard problems across a variety of industries by teaching machines to see and understand the world. Since 2008, the company has applied its patented capabilities to serve a variety of industries from advertising to professional sports, with more to come."
   },
   {
       src: "NuORDER.png",
       alt: "NuORDER",
-      url: "https://www.nuorder.com",
+      url: "www.nuorder.com",
       body: "NuORDER is a venture-backed, B2B eCommerce technology company revolutionizing the way the $130 billion wholesale business is done. Since 2011, we’ve offered a platform to empower brands and retailers to conduct their business in a smarter, modern, more efficient way and 100% online."
   },
  {
       src: "disqo.png",
       alt: "DISQO",
-      url: "https://www.disqo.com",
+      url: "www.disqo.com",
       body: "DISQO is a consumer-first insights platform that connects what people say and do to provide unprecedented insights for the market research industry.  By engaging our first-party research audience to share their attitudes and behaviors, DISQO captures the highest quality data, empowering its clients to make confident decisions."
   },
   {
       src: "ephesoft.png",
       alt: "Ephesoft",
-      url: "https://www.esphesoft.com",
+      url: "www.esphesoft.com",
       body: "Ephesoft makes meaning out of unstructured data through document capture and extraction backed up by practical experience and solid technology. Ephesoft drives automation through the Cloud, and Ephesoft is committed to leading the market with revolutionary technology that combines content automation, AI and machine learning."
   }
 ];
@@ -56,49 +56,49 @@ var bronzeSponsors = [
   {
       src: "getaround.png",
       alt: "Getaround",
-      url: "https://www.getaround.com",
+      url: "www.getaround.com",
       body: "Getaround is a carsharing platform powered by the Getaround Connect® technology which provides drivers access to rent and unlock cars shared by people nearby."
   },
   {
       src: "teamplayer.jpg",
       alt: "TeamPlayerHR",
-      url: "https://www.teamplayerhr.com",
+      url: "www.teamplayerhr.com",
       body: "TeamPlayerHR is a revolutionary website tool that dramatically reduces the problem of bad hires, and poor team member compatibility. TeamPlayerHR has its clear focus on helping employers drive high performance team building and retention. It has been estimated that bad hires and poor team member compatibility can cost £32,000 per employee in recruitment fees, wasted wages, loss of productivity and time. It is further estimated that management and supervisors can waste up to 46% of their time on HR issues and that bad compatibility and fit mean that a high percentage of employees may be seeking new jobs at any one time. "
   },
   {
       src: "springlabs.jpg",
       alt: "Spring Labs",
-      url: "https://www.springlabs.com",
+      url: "www.springlabs.com",
       body: "Spring Labs is building a protocol focused on protecting consumer data for the new age of information exchange and privacy."
   },
   {
       src: "sabio.png",
       alt: "Sabio Coding Bootcamp",
-      url: "https://www.sabio.la",
+      url: "www.sabio.la",
       body: "Sabio Coding Bootcamp"
   },
   {
       src: "freezecrowd.jpg",
       alt: "FreezeCrowd, Inc.",
-      url: "https://www.freezecrowd.com",
+      url: "www.freezecrowd.com",
       body: "FreezeCrowd is a unique social media platform and network for college students and alumni that connects you in photos with friends! It's cool or unique because everyone has an ice cube over their profile picture, and you 'break the ice' when visiting a friends profile.  By breaking the ice, you can ask a question or play an educational game, among other interesting applications that can be built over the break the ice feature.  FreezeCrowd is similar to a school yearbook in some ways, yet private to outside searches.  Every photo is unique like a snowflake and categorized!"
   },
   {
       src: "clutter.png",
       alt: "Clutter",
-      url: "https://www.clutter.com",
+      url: "www.clutter.com",
       body: "Clutter is an on-demand, technology company based in Los Angeles that is disrupting the $50B/year self-storage and moving industries. We’ve built an end-to-end logistics and supply chain platform that enables us to offer consumers a much more convenient solution at price parity with the incumbents. We’ve raised $300M from a number of VCs, including SoftBank, Sequoia, Atomico and GV (formerly Google Ventures). We have 800+ team members and tens of thousands of customers in 7 major markets across the US with plans to be in 50+ markets, domestically and internationally, within the next 5 years! At Clutter, we're fortunate to be providing a consumer value proposition that people love and one that makes economic sense - a true product/market fit that few startups ever find. To deliver on our promise to consumers, team members and investors, we're focused on hiring, training and retaining exceptional individuals. This means that we have a very thorough interview process and maintain high performance expectations, but we'll always be transparent with you and respectful of your time."
   },
   {
       src: "blisspoint.png",
       alt: "Bliss Point Media",
-      url: "https://www.blisspointmedia.com",
+      url: "www.blisspointmedia.com",
       body: "Bliss Point Media is a rapidly growing performance media advertising agency, based in Santa Monica that combines world-class media capabilities with cutting-edge statistics and computer science. Using technology as a differentiator, Bliss Point Media brings modern statistical and computational techniques to bear on our clients’ biggest marketing opportunities across linear TV and streaming/OTT video platforms. Our approach provides our clients with unique and powerful tools to maximize the effectiveness of their advertising campaigns while providing an experience of strategic depth and flawless execution."
   },
   {
       src: "ariento.png",
       alt: "Ariento Inc.",
-      url: "https://www.ariento.com",
+      url: "www.ariento.com",
       body: "Ariento provides secure technology solutions to small businesses and individuals. Specifically, our core services of Cyber Lookout, Secure Workstation, and Secure Office."
   },
   {
@@ -110,13 +110,13 @@ var bronzeSponsors = [
   {
       src: "onebox.png",
       alt: "OneBox LTD",
-      url: "https://www.onebox.com/signup/",
+      url: "www.onebox.com/signup/",
       body: "OneBox uses AI SaaS to increase container shipping efficiency by 20%, increasing logistics company profitability and reducing carbon emissions on a global scale."
   },
   {
       src: "deep6AI.png",
       alt: "Deep 6 AI",
-      url: "https://www.deep6.ai",
+      url: "www.deep6.ai",
       body: "We are an AI company that has created a new industry called Clinical Trial Acceleration Software.  We help bring life-saving drugs to market faster and lower the costs of these medications.  We just made Forbes 2019 Top 50 AI Companies list, we have a great development culture, a team of top talent, we just raised our Series A, and we are about to open our new office in Pasadena."
   },
   {
@@ -128,25 +128,25 @@ var bronzeSponsors = [
   {
       src: "need.png",
       alt: "Need Westwood",
-      url: "https://www.needwestwood.com",
+      url: "www.needwestwood.com",
       body: "We deliver your favorite snacks in under 10 minutes using electric vehicles! Customers order on our website needwestwood.com while we are building the app."
   },
   {
       src: "sike.png",
       alt: "Sike AI",
-      url: "https://www.sikeinsights.com",
+      url: "www.sikeinsights.com",
       body: "Sike is an enterprise software company that's combining psychology and machine learning to help teams work better together."
   },
   {
       src: "PEARL.png",
       alt: "Pearl",
-      url: "https://www.trypearl.io",
+      url: "www.trypearl.io",
       body: "Pearl is a job platform for young professionals to discover, follow, & apply to amazing new companies; and for those companies to represent themselves with more transparent information about their values, culture, and mission statement."
   },
  {
       src: "textbookdibs.png",
       alt: "TextbookDibs",
-      url: "https://www.textbook-dibs.herokuapp.com",
+      url: "www.textbook-dibs.herokuapp.com",
       body: "Textbook Dibs is the most convenient way to resell your textbooks."
   }
 
@@ -160,7 +160,7 @@ function eachFun(company, title) {
         <a class="sponsor-popover" tabindex="0" data-toggle="popover" data-placement="top" title="" data-html="true"
           data-content=
             "<p>` + company.body + ` Learn more at
-            <a style='word-wrap: break-word;' target='_blank' href='company.url'> ` + company.url +`</a>.</p>
+            <a style='word-wrap: break-word;' target='_blank' href='http://` + company.url +`'> ` + company.url +`</a>.</p>
             <p>Positions: ` + company.positions + `</p>" data-original-title="` + company.alt + `">
           <img src="assets/logos/` + company.src +  `" alt="` + company.alt + `"/></a>
       </div>`;
@@ -171,7 +171,7 @@ function eachFun(company, title) {
       <a class="sponsor-popover" tabindex="0" data-toggle="popover" data-placement="top" title="" data-html="true"
         data-content=
           "<p>` + company.body + ` Learn more at
-          <a style='word-wrap: break-word;' target='_blank' href='company.url'> ` + company.url +`</a>.</p>"
+          <a style='word-wrap: break-word;' target='_blank' href='http://` + company.url +`'> ` + company.url +`</a>.</p>"
           data-original-title="` + company.alt + `">
         <img src="assets/logos/` + company.src +  `" alt="` + company.alt + `"/></a>
     </div>`;
